d54dc44870f28455ef1b26260fd1384c7b2f7ddd
sho037 <103867061+sho037@users.noreply.github.com>
2024-02-13 15:09:38 +0000

    Lib：tailwindcss導入

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
     </div>
   );
 }
