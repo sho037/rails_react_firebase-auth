32a8df44a11dacaa1f4db3ea02227b032c6b7846
sho037 <103867061+sho037@users.noreply.github.com>
2024-02-13 14:57:39 +0000

    Feature：Firebaseを用いたユーザー登録API作成

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
+require "firebase_authenticator.rb"
+
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  def authenticate
+    token = request.headers['Authorization']&.split('Bearer ')&.last
+    return render json: { status: "error", error: 'Unauthorized' }, status: :unauthorized unless token
+
+    begin
+      payload = FirebaseAuthenticator.new(token).validate!
+      @current_user = User.find_by(uid: payload['sub'])
+    rescue FirebaseAuthenticator::InvalidTokenError => e
+      return render json: { status: "error", error: e.message }, status: :unauthorized
+    end
+  end
+
+  def current_user
+    @current_user
+  end
+
+  def logged_in?
+    !!@current_user
+  end
 end
---
+++ api/app/controllers/v1/users_controller.rb
@@ -0,0 +1,22 @@
+require "firebase_authenticator.rb"
+
+class V1::UsersController < ApplicationController
+  skip_before_action :authenticate, only: [:create]
+
+  def create
+    token = request.headers['Authorization']&.split('Bearer ')&.last
+    return render json: { status: "error", error: 'Unauthorized' }, status: :unauthorized unless token
+
+    begin
+      payload = FirebaseAuthenticator.new(token).validate!
+      @user = User.find_or_create_by(uid: payload['sub']) do |user|
+        user.nickname = payload['nickname']
+      end
+    rescue FirebaseAuthenticator::InvalidTokenError => e
+      return render json: { error: e.message }, status: :unauthorized
+    end
+
+    render json: { status: "success", user: @user }
+  end
+
+end
